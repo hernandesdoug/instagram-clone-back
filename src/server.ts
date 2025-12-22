f3d92b289f4c33b52cfe9f3067ea62de1f9c410e
Douglas Hernandes <douglashernandes@icloud.com>
2025-12-22 17:26:26 -0300

    configurando servidor inicial

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, response) => {
+    response.send("Hello World!");
+} );
+
+app.listen(3333, () => {
+    console.log("Servidor rodando");
+});
