683e8fe72ef4645c140bed9799be354e2246c8c2
Douglas Hernandes <douglashernandes@icloud.com>
2026-01-13 07:28:18 -0300

    Correcao do upload e configuracao da roda

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,23 @@
 import express from "express";
 import {postUserByLogin, postUser} from "../controllers/userController";
+import multer from "multer";
+import fs from "fs";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        const uploadDir = "./uploads";
+        if (!fs.existsSync(uploadDir)) {
+            fs.mkdirSync(uploadDir);
+        }
+        cb(null, uploadDir);
+    },
+    filename: function (req, file, cb) {
+        const filename = file?.originalname.split(" ").join("-");
+        cb(null, filename);
+    }
+
+})
+const upload = multer({ storage })
 
 const userRoutes = express.Router();
 
@@ -7,9 +25,9 @@ const userRoutes = express.Router();
 
 userRoutes.post("/user/login", postUserByLogin);
 
-userRoutes.post("/user", postUser);
+userRoutes.post("/user", upload.single('avatar'), postUser);
 
-//userRoutes.put("/user/:id", updateUser);
+//userRoutes.put("/user/:id", upload.single('avatar'), updateUser);
 
 //userRoutes.delete("/user/:id", deleteUser);
 
